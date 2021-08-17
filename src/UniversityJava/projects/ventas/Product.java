c01e434801d5134e17f333ad1e5d6b7bfd729df5
Chrisxavix <chrischillogallid64@outlook.com>
2021-08-16 23:17:16 -0500

    feat: add projects

---
+++ src/UniversityJava/projects/ventas/Product.java
@@ -0,0 +1,8 @@
+package UniversityJava.projects.ventas;
+
+public class Product {
+    private /*final*/ int idProducto;
+    private String nombre;
+    private double precio;
+    private static int contadorProductos;
+}

ecab7615ed2daf2323bb3bf8d629032a09b33aa3
Chrisxavix <chrischillogallid64@outlook.com>
2021-08-22 10:47:53 -0500

    feat: sobreescritura en herencia

---
+++ src/UniversityJava/topics/overrideherency/TestSobreescritura.java
@@ -0,0 +1,8 @@
+package UniversityJava.topics.overrideherency;
+
+public class TestSobreescritura {
+    public static void main(String[] args) {
+        Gerente gerente1 = new Gerente("Juan", 5000, "Sistemas");
+        System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+    }
+}
